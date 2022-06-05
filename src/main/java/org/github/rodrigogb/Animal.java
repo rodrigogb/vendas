e79241f877c0800a7f46defa34eec868bdeb6caa
rodrigogb <rodrigogbuzato@hotmail.com>
2022-06-05 14:08:10 -0300

    primeiro commit

---
+++ src/main/java/org/github/rodrigogb/Animal.java
@@ -0,0 +1,5 @@
+package org.github.rodrigogb;
+
+public interface Animal {
+    void fazerBarulho();
+}
---
+++ src/main/java/org/github/rodrigogb/MainConfiguration.java
@@ -0,0 +1,11 @@
+package org.github.rodrigogb;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class MainConfiguration {
+
+}
