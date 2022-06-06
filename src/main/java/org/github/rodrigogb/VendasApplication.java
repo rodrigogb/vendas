ec4ec6a34c6a7ec467a71c45a013d735aa8d2900
rodrigogb <rodrigogbuzato@hotmail.com>
2022-06-06 14:32:07 -0300

    inicio do cadastro de clientes

--- src/main/java/org/github/rodrigogb/VendasApplication.java
+++ src/main/java/org/github/rodrigogb/VendasApplication.java
@@ -1,12 +1,30 @@
 package org.github.rodrigogb;
 
+import org.github.rodrigogb.domain.entity.Cliente;
+import org.github.rodrigogb.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes) {
+        return args -> {
+            clientes.salvar(new Cliente("Rodrigo"));
+            clientes.salvar(new Cliente("Douglas"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+        };
+    }
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
